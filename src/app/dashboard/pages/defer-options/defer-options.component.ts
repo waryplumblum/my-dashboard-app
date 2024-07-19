8e72871ec6fa22c439e6e629693d51e676b6d331
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-19 17:00:12 -0600

    feat: Defer Triggers - Interactions

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders-slow/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersFastComponent,TitleComponent],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders-slow/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-fast.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
-  template: `<h1>Hola Mundo</h1>`,
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full', cssClass ]">
+      <<ng-content />
+    </section>
+  `,
 })
 export class HeavyLoadersFastComponent {
+
+  @Input({ required: true }) cssClass!:string;
 
 }
