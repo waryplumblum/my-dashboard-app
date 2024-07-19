f71575d0e704e182d8407ec8bb49bd9ccfa05ad2
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-19 10:44:04 -0600

    feat: Structure from directories

---
+++ src/app/dashboard/pages/view-transition/view-transition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-transition',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-transition.component.html',
+  styles: ``
+})
+export class ViewTransitionComponent {
+
+}
