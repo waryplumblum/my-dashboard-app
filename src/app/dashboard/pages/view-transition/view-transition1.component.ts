ac81b156e93fdba915f431d6e1e55c5b84008e2c
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-19 18:09:19 -0600

    feat: ViewTransitions

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -9,8 +9,8 @@ import { CommonModule } from '@angular/common';
   template: `
     <app-title title="View Transition 1"/>
     <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"/>
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"style="view-transition-name: hero1"/>
+      <div class="bg-blue-500 w-56 h-56"style="view-transition-name: hero2"></div>
     </section>
   `,
 })
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,9 +8,9 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule,TitleComponent],
   template: `
     <app-title title="View Transition 2"/>
-    <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"/>
-      <div class="bg-blue-500 w-56 h-56"></div>
+    <section class="flex justify-end">
+      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"style="view-transition-name: hero1"/>
+      <div class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"style="view-transition-name: hero2"></div>
     </section>
   `,
 })
