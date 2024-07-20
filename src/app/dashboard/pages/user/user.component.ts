eb268caf03ea161fdd8dd40131cb024a4ac753f6
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-19 18:19:06 -0600

    feat: Service with signals

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '@services/users.service';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export default class UserComponent {
+
+  public userService = inject(UsersService);
 
 }
