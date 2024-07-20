cb7f23c6fadcaad165200dc0b329aec04798379f
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-20 00:00:39 -0600

    feat: Use component form angular material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule,MatIconModule,MatButtonModule,MatBadgeModule],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
